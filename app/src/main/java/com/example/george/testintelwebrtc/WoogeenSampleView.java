a5d78a07df97a296d17c7ffc18ff27babd93c457
George Vigelette <gvigelet@duvitech.com>
2016-08-01 00:10:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/george/testintelwebrtc/WoogeenSampleView.java
@@ -0,0 +1,12 @@
+package com.example.george.testintelwebrtc;
+
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class WoogeenSampleView extends GLSurfaceView {
+
+    public WoogeenSampleView(Context context){
+        super(context);
+    }
+}
